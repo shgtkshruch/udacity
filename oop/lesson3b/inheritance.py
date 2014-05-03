e542f9faba5ab0cb03450b625e1e815b6715667b
shgtkshruch <shgtk.shruch@gmail.com>
2014-05-03 18:59:01 +0900

    (Lesson 3b)Inheritance and Method Overriding

---
+++ oop/lesson3b/inheritance.py
@@ -0,0 +1,25 @@
+class Parent():
+    def __init__(self, last_name, eye_color):
+        print("Parent Constructor Called")
+        self.last_name = last_name
+        self.eye_color = eye_color
+
+    def show_info(self):
+        print("Last Name - " + self.last_name)
+        print("Eye color - " + self.eye_color)
+
+class Child(Parent):
+    def __init__(self, last_name, eye_color, number_of_toys):
+        print("Child Constructor Called")
+        Parent.__init__(self, last_name, eye_color)
+        self.number_of_toys = number_of_toys
+
+    def show_info(self):
+        print("Last Name - " + self.last_name)
+        print("Eye color - " + self.eye_color)
+        print("Numberof toys - " + str(self.number_of_toys))
+
+billy_cyrus = Parent("Cyrus", "blue")
+billy_cyrus.show_info()
+miley_cyrus = Child("Cyrus", "Blue", 5)
+miley_cyrus.show_info()
