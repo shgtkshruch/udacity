05bfe0d474ab3435cd8638fae5d17057954d802e
shgtkshruch <shgtk.shruch@gmail.com>
2014-05-08 13:36:11 +0900

    (Lesson 2) Probsem Set (Optional)
    Add Question.1 weekend
    
    Add Question.2 stamps
    
    Add Question.3 range of a set

---
+++ ics/lesson2/problem_set_optional/weekend.py
@@ -0,0 +1,15 @@
+# Define a procedure weekend which takes a string as its input, and
+# returns the boolean True if it's 'Saturday' or 'Sunday' and False otherwise.
+
+def weekend(day):
+    # your code here
+    return day == 'Saturday' or day == 'Sunday'
+    
+print weekend('Monday')
+#>>> False
+
+print weekend('Saturday')
+#>>> True
+
+print weekend('July')
+#>>> False
